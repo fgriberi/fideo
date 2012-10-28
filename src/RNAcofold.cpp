cf1001580a6abddde5567fbd83040783bb5500ec
Franco Riberi <fgriberi@gmail.com>
2012-10-28 15:11:14 -0300

    Add RNAcofold backends

---
+++ src/RNAcofold.cpp
@@ -0,0 +1,105 @@
+/*
+ * File:   RNAcofold.cpp
+ * Author: Franco Riberi <fgriberi at gmail.com>
+ *
+ * Created on October 28, 2012, 10:32 AM
+ *
+ * Copyright (C) 2012 Franco Riberi, FuDePAN
+ *
+ * This file is part of fideo.
+ *
+ * fideo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * fideo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with fideo. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include <sstream>
+#include <string>
+#include <mili/mili.h>
+#include "fideo/IHybridize.h"
+#include "fideo/RNABackendProxy.h"
+
+using namespace biopp;
+using namespace mili;
+using namespace std;
+
+//Vienna package
+class RNAcofold : public IHybridize
+{
+    virtual Fe hybridize(const NucSequence& firstSeq, const NucSequence& secondSeq) const;
+
+    class ParseBody
+    {
+        enum Columns
+        {
+            ColRNAcofoldResult,
+            ColdG,
+            NumberOfColumns
+        };
+
+    public:
+        static const size_t DELTA_G = 1;
+        Fe dG;
+
+        void parse(string& line)
+        {
+            stringstream ss(line);
+            vector<string> result;
+            ss >> mili::Separator(result, ' ');            
+            if (result.size() != NumberOfColumns)
+                throw RNABackendException("Invalid output RNAup.");
+            string deltaG = result[DELTA_G].substr(1, result[DELTA_G].size()-2);
+            from_string(deltaG, dG);
+        }
+    };
+};
+
+static const string FILE_NAME_OUTPUT = "outputRNAcofold.out";
+
+REGISTER_FACTORIZABLE_CLASS(IHybridize, RNAcofold, std::string, "RNAcofold");
+
+Fe RNAcofold::hybridize(const NucSequence& firstSeq, const NucSequence& secondSeq) const
+{
+    string seq1;
+    for (size_t i = 0; i < firstSeq.length(); ++i)
+        seq1 += firstSeq[i].as_char();
+    string seq2;
+    for (size_t i = 0; i < secondSeq.length(); ++i)
+        seq2 += secondSeq[i].as_char();
+
+    stringstream cmd1;
+    ofstream toHybridize("toHybridize");
+    toHybridize << seq1 << "&" << seq2;
+    toHybridize.close();
+
+    stringstream cmd2;
+    cmd2 << "RNAcofold ";
+    cmd2 << "< " << "toHybridize ";
+    cmd2 << "> " << FILE_NAME_OUTPUT;
+
+    const Command CMD2 = cmd2.str();  //RNAup -u 3,4 -c SH < toHybridize > FILE_NAME_OUTPUT
+    runCommand(CMD2);
+
+    ifstream fileOutput(FILE_NAME_OUTPUT.c_str());
+    if (!fileOutput)
+        throw RNABackendException("Output file not found.");
+
+    string temp;        
+    getline(fileOutput,temp);
+    getline(fileOutput,temp);
+               
+    ParseBody body;
+    body.parse(temp);
+    remove_file(FILE_NAME_OUTPUT.c_str());
+    return body.dG;
+}
---
+++ test/RNAcofoldTest.cpp
@@ -0,0 +1,42 @@
+/*
+    Copyright (C) 2012 Franco Riberi, FuDePAN
+
+    This file is part of the Fideo Library.
+
+    Fideo is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Fideo is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Fideo. If not, see <http://www.gnu.org/licenses/>.
+
+    This is a test file.
+*/
+#include <string>
+#include <fstream>
+#include <fideo/fideo.h>
+#include <biopp/biopp.h>
+#include <mili/mili.h>
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+TEST(RNAcofoldBackendTestSuite, BasicTest)
+{
+    const biopp::NucSequence seq1("GGAGUAGGUUAUCCUCUGUU");
+    const biopp::NucSequence seq2("AGGACAACCU");
+
+    IHybridize* p = mili::FactoryRegistry<IHybridize, std::string>::new_class("RNAcofold");
+    ASSERT_TRUE(p != NULL);
+
+    double dG = p->hybridize(seq1, seq2);
+    ASSERT_DOUBLE_EQ(dG, -10);
+    delete p;
+
+    EXPECT_FALSE(std::ifstream("outputRNAcofold.out"));
+}

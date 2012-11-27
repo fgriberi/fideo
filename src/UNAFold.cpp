35a6aa01cda1e58217b6536b713f17943938f2af
Franco Riberi <fgriberi@gmail.com>
2012-11-27 19:38:15 -0300

    delete unnecessary parentheses in UNAFold.cpp

--- src/UNAFold.cpp
+++ src/UNAFold.cpp
@@ -121,16 +121,16 @@ REGISTER_FACTORIZABLE_CLASS(IFold, UNAFold, std::string, "UNAFold");
 
 void UNAFold::delete_all_files()
 {
-    remove_file((get_input_file_name() + ".ct"));
-    remove_file((get_input_file_name() + ".dG"));
-    remove_file((get_input_file_name() + ".h-num"));
-    remove_file((get_input_file_name() + ".log"));
-    remove_file((get_input_file_name() + ".plot"));
-    remove_file((get_input_file_name() + ".run"));
-    remove_file((get_input_file_name() + ".ss-count"));
-    remove_file((get_input_file_name() + ".ann"));
-    remove_file((get_input_file_name() + ".det"));
-    remove_file((get_input_file_name()));
+    remove_file(get_input_file_name() + ".ct");
+    remove_file(get_input_file_name() + ".dG");
+    remove_file(get_input_file_name() + ".h-num");
+    remove_file(get_input_file_name() + ".log");
+    remove_file(get_input_file_name() + ".plot");
+    remove_file(get_input_file_name() + ".run");
+    remove_file(get_input_file_name() + ".ss-count");
+    remove_file(get_input_file_name() + ".ann");
+    remove_file(get_input_file_name() + ".det");
+    remove_file(get_input_file_name());
 }
 
 

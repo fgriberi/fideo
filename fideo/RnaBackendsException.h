342f7edd1d7456ce76d54ba4095a337507c1b9ff
Franco Riberi <fgriberi@gmail.com>
2013-05-18 18:34:27 -0300

    Deleted unused exceptions

--- fideo/RnaBackendsException.h
+++ fideo/RnaBackendsException.h
@@ -50,40 +50,28 @@ class RNABackendExceptionRoot {};
 
 DEFINE_SPECIFIC_EXCEPTION_TEXT(RNABackendException, RNABackendExceptionRoot, "RNABackend");
 
-DEFINE_SPECIFIC_EXCEPTION_TEXT(InvalidStructureException, RNABackendExceptionRoot, "Invalid structure");
-
-DEFINE_SPECIFIC_EXCEPTION_TEXT(CombinatorException, RNABackendExceptionRoot, "Out of range");
-
-DEFINE_SPECIFIC_EXCEPTION_TEXT(SeparatorException, RNABackendExceptionRoot, "Error in string parser");
-
-DEFINE_SPECIFIC_EXCEPTION_TEXT(FileReadException, RNABackendExceptionRoot, "The config file couldn't be read");
-
 DEFINE_SPECIFIC_EXCEPTION_TEXT(FromStringException, RNABackendExceptionRoot, "Wrong column type");
 
-///FideoHelper exceptions
 DEFINE_SPECIFIC_EXCEPTION_TEXT(NotReadValueUsingOffsetException, RNABackendExceptionRoot, "Could not read the value from given line, offset and length");
 
 DEFINE_SPECIFIC_EXCEPTION_TEXT(NotReadValueException, RNABackendExceptionRoot, "Could not read the value from given line");
 
-DEFINE_SPECIFIC_EXCEPTION_TEXT(SystemCallException, RNABackendExceptionRoot, "System call failed");
+DEFINE_SPECIFIC_EXCEPTION_TEXT(InvalidStructureException, RNABackendExceptionRoot, "Invalid structure");
 
-DEFINE_SPECIFIC_EXCEPTION_TEXT(NonTerminationException, RNABackendExceptionRoot, "Non termination for some reason");
+DEFINE_SPECIFIC_EXCEPTION_TEXT(SeparatorException, RNABackendExceptionRoot, "Error in string parser");
 
-//IntaRNA
 DEFINE_SPECIFIC_EXCEPTION_TEXT(UnsupportedException, RNABackendExceptionRoot, "Unsupported Sequence");
-
-DEFINE_SPECIFIC_EXCEPTION_TEXT(InvalidPathException, RNABackendExceptionRoot, "Invalid path of IntaRNA executable");
 
 DEFINE_SPECIFIC_EXCEPTION_TEXT(NotFoundFileException, RNABackendExceptionRoot, "Output file not found");
 
 DEFINE_SPECIFIC_EXCEPTION_TEXT(ExceptionUnlink, RNABackendExceptionRoot, "Error unlink");
 
-//UNAFold
 DEFINE_SPECIFIC_EXCEPTION_TEXT(InvalidMotif, RNABackendExceptionRoot, "Invalid motif");
 
 DEFINE_SPECIFIC_EXCEPTION_TEXT(FileNotExist, RNABackendExceptionRoot, "The .det file not exist");
 
 DEFINE_SPECIFIC_EXCEPTION_TEXT(IndexOutOfRange, RNABackendExceptionRoot, "Index out of range");
+
 
 }// namespace fideo
 #endif  /* _RNA_BACKENDS_EXCEPTIONS_H */
